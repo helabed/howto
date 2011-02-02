bcbe58517eac62adf6f0f94e65b2c3b38dfe3049
Hani Elabed <hani.elabed@gmail.com>
2011-02-01 21:23:45 -0600

    removed disk cp for sake of herouku

--- vendor/plugins/active_scaffold/lib/active_scaffold/bridges/date_picker/bridge.rb
+++ vendor/plugins/active_scaffold/lib/active_scaffold/bridges/date_picker/bridge.rb
@@ -7,9 +7,9 @@ ActiveScaffold::Bridges.bridge "DatePicker" do
     destination = File.join(Rails.root, "public/javascripts/active_scaffold/default/")
 
     if ActiveScaffold.js_framework == :jquery
-      require File.join(directory, "lib/datepicker_bridge.rb")
-      FileUtils.cp(source, destination)
-      ActiveScaffold::Bridges::DatePickerBridge.localization(File.join(destination, 'date_picker_bridge.js'))
+      #require File.join(directory, "lib/datepicker_bridge.rb")
+      #FileUtils.cp(source, destination)
+      #ActiveScaffold::Bridges::DatePickerBridge.localization(File.join(destination, 'date_picker_bridge.js'))
     else
       # make sure that jquery files are removed
       FileUtils.rm(File.join(destination, 'date_picker_bridge.js')) if File.exist?(File.join(destination, 'date_picker_bridge.js'))
